399e3551f6ef18734df2b1db58d6b3f19ecb2c51
Drexel7 <126233788+Drexel7@users.noreply.github.com>
2025-05-11 23:32:38 +0300

    Update simple_dtw.m

--- src/simple_dtw.m
+++ src/simple_dtw.m
@@ -1,12 +1,13 @@
+%d = scorul final (costul minim de deformare intre cele douz semnale)
+%path = lista de potriviri (indexi corespondenti intre s si t) pentru a le alinia cat mai bine
+
 function [d, path] = simple_dtw(s, t)
     ns = length(s);
     nt = length(t);
 
-    % Inițializare matrice DTW
     D = inf(ns+1, nt+1);
     D(1,1) = 0;
 
-    % Populare matrice
     for i = 2:ns+1
         for j = 2:nt+1
             cost = abs(s(i-1) - t(j-1));
@@ -14,17 +15,18 @@ function [d, path] = simple_dtw(s, t)
         end
     end
 
-    d = D(end,end);  % distanța totală DTW
+    d = D(end,end);  
 
-    % ==========================
-    % RECONSTRUCȚIE PATH
-    % ==========================
+
+    % path - calea optima de aliniere calculata de algoritmul DTW, adica ce element 
+    % din semnalul test corespunde fiecarui element din eticheta vocala, tinand cont 
+    % de diferentele de durata, intindere sau decalaj.
     i = ns + 1;
     j = nt + 1;
     path = [];
 
     while i > 1 && j > 1
-        path = [ [i-1, j-1]; path ];  % salvează perechea (index în s, index în t)
+        path = [ [i-1, j-1]; path ];  
         [~, idx] = min([D(i-1,j-1), D(i-1,j), D(i,j-1)]);
         switch idx
             case 1
@@ -37,6 +39,5 @@ function [d, path] = simple_dtw(s, t)
         end
     end
 
-    % Adaugă punctul de start
     path = [[1,1]; path];
 end
